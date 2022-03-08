e940be1f8243379dcc7431b770c6703c6bb6804f
Ryan Tjoa <tjoa@g.ucla.edu>
2022-03-08 15:18:37 -0800

    Subtraction GCD: fix bug where a=0,b!=0, add tests

--- examples/gcd_subtraction.jl
+++ examples/gcd_subtraction.jl
@@ -1,65 +1,103 @@
 using Revise
 using Dice
 
-# 4 has probability 0.3, etc...
-dist_vals = [4, 6, 9]
-dist_weights = [0.3, 0.2, 0.5]
-
-# Convert to input for dice discrete distribution
-dice_disc = zeros(maximum(dist_vals) + 1)
-for (val, weight) in zip(dist_vals, dist_weights)
-    dice_disc[val + 1] = weight
+# Convert (value, weight) tuples to input for dice discrete distribution
+# Example: unpack_sparse([(0, 0.2), (3, 0.8)]) => [0.2, 0, 0, 0.8]
+function unpack_sparse(sparse::Vector{Tuple{Int,Float64}})
+    dice_disc = zeros(maximum(val for (val, _) in sparse) + 1)
+    for (val, weight) in sparse
+        dice_disc[val + 1] = weight
+    end
+    return dice_disc
 end
 
-# Naively calculate GCD(discrete(dice_disc), discrete(dice_disc))
-gcd_dist = Dict()
-for (val1, weight1) in zip(dist_vals, dist_weights)
-    for (val2, weight2) in zip(dist_vals, dist_weights)
-        res = gcd(val1, val2)
-        if !haskey(gcd_dist, res)
-            gcd_dist[res] = 0
+# Naively calculate P[gcd(X, Y) = res] given distributions of X and Y
+function gcd_enumeration(
+    sparse1::Vector{Tuple{Int,Float64}},
+    sparse2::Vector{Tuple{Int,Float64}},
+    res::Int
+)
+    total_weight = 0
+    for (val1, weight1) in sparse1
+        for (val2, weight2) in sparse2
+            if gcd(val1, val2) == res
+                total_weight += weight1 * weight2
+            end
         end
-        gcd_dist[res] += weight1 * weight2
     end
+    return total_weight
 end
 
-@show gcd_dist
+# Generate Dice code for P[gcd(X, Y) = res] given distributions of X and Y
+function code_gcd(
+    sparse1::Vector{Tuple{Int,Float64}},
+    sparse2::Vector{Tuple{Int,Float64}},
+    res::Int
+)
+    discrete_dist1 = unpack_sparse(sparse1)
+    discrete_dist2 = unpack_sparse(sparse2)
+    @dice begin
+        function discrete(p::Vector{Float64})
+            mb = length(p)
+            v = Vector(undef, mb)
+            sum = 1
+            for i=1:mb
+                v[i] = p[i]/sum
+                sum = sum - p[i]
+            end
 
-code = @dice begin
-    function discrete(p::Vector{Float64})
-        mb = length(p)
-        v = Vector(undef, mb)
-        sum = 1
-        for i=1:mb
-            v[i] = p[i]/sum
-            sum = sum - p[i]
+            ans = DistInt(dicecontext(), mb-1)
+            for i=mb-1:-1:1
+                ans = if flip(v[i]) DistInt(dicecontext(), i-1) else ans end
+            end
+            return ans
         end
 
-        ans = DistInt(dicecontext(), mb-1)
-        for i=mb-1:-1:1
-            ans = if flip(v[i]) DistInt(dicecontext(), i-1) else ans end
-        end
-        return ans
-    end
-
-    # Subtraction-based GCD
-    # https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations
-    function gcd(a::Tuple{DistInt, DistBool}, b::Tuple{DistInt, DistBool})
-        for _ = 0:100  # TODO: better bound
-            gt = a[1] > b[1]
+        # Subtraction-based GCD
+        # https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations
+        function dice_gcd(a::Tuple{DistInt, DistBool}, b::Tuple{DistInt, DistBool})
+            # Swap if a < b to handle cases where a=0 and b!=0
             lt = b[1] > a[1]
-            amb = a[1] - b[1]
-            bma = b[1] - a[1]
-            a = Dice.ifelse(gt, amb[1], a[1]), a[2] | (amb[2] & gt)
-            b = Dice.ifelse(lt, bma[1], b[1]), b[2] | (bma[2] & lt)
+            t = a
+            a = Dice.ifelse(lt, b[1], a[1]), Dice.ifelse(lt, b[2], a[2])
+            b = Dice.ifelse(lt, t[1], b[1]), Dice.ifelse(lt, t[2], b[2])
+            
+            for _ = 0:100  # TODO: better bound
+                gt = a[1] > b[1]
+                lt = b[1] > a[1]
+                amb = a[1] - b[1]
+                bma = b[1] - a[1]
+                a = Dice.ifelse(gt, amb[1], a[1]), a[2] | (amb[2] & gt)
+                b = Dice.ifelse(lt, bma[1], b[1]), b[2] | (bma[2] & lt)
+            end
+            return a[1], a[2]
         end
-        return a[1], a[2]
-    end
 
-    a = discrete(dice_disc), flip(0)
-    b = discrete(dice_disc), flip(0)
-    g = gcd(a, b)
-    prob_equals(g[1], 2) & !g[2]
+        a = discrete(discrete_dist1), flip(0)
+        b = discrete(discrete_dist2), flip(0)
+        g = dice_gcd(a, b)
+        prob_equals(g[1], res) & !g[2]
+    end
 end
 
-infer(code, :bdd)
+all_pass = true
+function test(sparse::Vector{Tuple{Int,Float64}})
+    for res = 0:10
+        code = code_gcd(sparse, sparse, res)
+        bdd = compile(code)
+        dice_p = infer(code, :bdd)
+        naive_p = gcd_enumeration(sparse, sparse, res)
+        if !isapprox(dice_p, naive_p)
+            global all_pass = false
+            println("FAIL. ", sparse, " ", res, " Expected: ", naive_p, " Got: ", dice_p)
+        end
+    end
+end
+
+sparse1 = [(4, 0.3), (6, 0.2), (9, 0.5)] # 4 has probability 0.3, etc...
+sparse2 = [(0, 0.2), (4, 0.3), (6, 0.5)]
+test(sparse1)
+test(sparse2)
+if all_pass
+    println("ALL TESTS PASSED")
+end
