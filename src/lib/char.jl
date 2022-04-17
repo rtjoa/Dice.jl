25e9f5a25dc41b085373c9280fca539c42db8db6
Ryan Tjoa <tjoa@g.ucla.edu>
2022-04-17 16:49:14 -0700

    String comparison

--- src/lib/char.jl
+++ src/lib/char.jl
@@ -34,9 +34,17 @@ function ifelse(cond::DistBool, then::DistChar, elze::DistChar)
     DistChar(cond.mgr, ifelse(cond, then.i, elze.i))
 end
 
-function Base.:>(x::DistChar, y::DistChar)
-    x.i > y.i
-end
-
 bools(c::DistChar) =
     bools(c.i)
+
+Base.:>(x::DistChar, y::DistChar) = x.i > y.i
+
+Base.:>(x::DistChar, y::Char) = x.i > DistChar(x.mgr, y).i
+
+Base.:>(x::Char, y::DistChar) = DistChar(y.mgr, x).i > y.i
+
+Base.:<(x::DistChar, y::DistChar) = y > x
+
+Base.:<(x::DistChar, y::Char) = y > x
+
+Base.:<(x::Char, y::DistChar) = y > x
